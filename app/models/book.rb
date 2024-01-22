98fccfe4774a36eaa258b7e3d56932bb8832c66d
Denio Barbosa Junior <deniojr@gmail.com>
2024-01-22 13:20:14 -0300

    removed unnecessary comments

--- app/models/book.rb
+++ app/models/book.rb
@@ -23,34 +23,3 @@ class Book < ApplicationRecord
     books
   end
 end
-
-
-
-=begin
-# app/models/book.rb
-class Book < ApplicationRecord
-  belongs_to :category
-  belongs_to :publisher
-
-  def self.filter_by_categories_and_publisher(category_names, publisher_name)
-    books = self.all
-
-    # Filter by categories
-    if category_names.present?
-      category_names = [category_names] unless category_names.is_a?(Array)
-      category_ids = Category.where('lower(name) IN (?)', category_names.map(&:downcase)).pluck(:id)
-      books = books.where(category_id: category_ids)
-    end
-
-  # Filter by publisher
-  if publisher_name.present?
-    publisher_name = [publisher_name] unless publisher_name.is_a?(Array)
-    publisher_ids = Publisher.where('lower(name) IN (?)', publisher_name.map(&:downcase)).pluck(:id)
-    books = books.where(publisher_id: publisher_ids)
-  end
-
-  books
-
-  end
-end
-=end
