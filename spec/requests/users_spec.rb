a858ce8e94d60ef5c5f926f08949db2b72f35393
Denio B. Junior <deniojr@gmail.com>
2024-02-26 08:33:57 -0300

    removed unecessary comments

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,27 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe RegistrationsController, type: :controller do
-#   describe 'POST /auth' do
-#     context 'with valid parameters' do
-#       it 'creates user with valid params' do
-#         expect {
-#           post :create, params: FactoryBot.build(:user)
-#         }
-#         expect(response.status).to eq(200)
-#       end
-#     end
-
-#     # test not passing
-#     context 'with invalid parameters' do
-#       it 'does not create a new user' do
-#         expect {
-#           post :create, params: FactoryBot.build(:user, name: '')
-#         }
-#         expect(response.status).to eq(200)
-#       end
-#     end
-#   end
-# end
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
@@ -32,7 +8,6 @@ RSpec.describe RegistrationsController, type: :controller do
   describe "POST /auth" do
     context 'with valid parameters' do
       it 'is valid with valid attributes' do
-        # user = FactoryBot.build(:user)
         @user = build(:user)
         expect(@user).to be_valid
       end
@@ -40,8 +15,6 @@ RSpec.describe RegistrationsController, type: :controller do
   end
 
   context 'with invalid parameters' do
-    # let(:invalid_attributes) { FactoryBot.attributes_for(user: { name: '', email: 'invalid', password: '123', password_confirmation: '321' } ) }
-    
     it 'does not create a new user' do
       @user = build(:user, :invalid_user)
       expect(@user).to_not be_valid
