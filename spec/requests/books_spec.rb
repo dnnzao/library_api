e2ec27ac64f4e80fd1039420388468eaa41f83d1
Denio B. Junior <deniojr@gmail.com>
2024-02-28 12:57:33 -0300

    RSpec tests for POST Books now working for logged in users

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -5,29 +5,40 @@ require 'rails_helper'
 RSpec.describe 'Books', type: :request do
   let!(:user) { create(:user) }
 
+  before(:each) do
+    @category = Category.find_or_create_by(id: 5, name: 'Fantasy')
+    @publisher = Publisher.find_or_create_by(id: 5, name: 'Penguin Random House')
+  end
+
+
+
   describe 'POST /books' do
     context 'when user is logged in' do
-      let(:valid_attributes) do {
-        book_name: 'The Factory Bot Book 2',
-        author: 'Factory Bot',
-        published_date: '2024-02-28',
-        category_id: 5,
-        publisher_id: 5
-      }
-      end
-
       it 'creates a new Book' do
         auth_headers = authenticate_user(user)
-        post books_path, params: { book: valid_attributes }.to_json, headers: auth_headers.merge('Content-Type' => 'application/json')
+        book_params = {
+          book_name: 'The Factory Bot Book 2',
+          author: 'Factory Bot',
+          published_date: Date.current,
+          category_id: @category.id,
+          publisher_id: @publisher.id
+        }
+        post books_path, params: { book: book_params }.to_json, headers: auth_headers.merge({'Content-Type' => 'application/json'})
         expect(response).to have_http_status(:created)
       end
-    end
 
-    # context 'with invalid parameters' do
-    #   it 'does not create a new Book' do
-    #     post books_path, params: invalid_book_attributes.to_json, headers: auth_headers.merge({'Content-Type' => 'application/json'})
-    #     expect(response).to have_http_status(:unprocessable_entity)
-    #   end
-    # end
+      it 'Books does not have valid parameters' do
+        auth_headers = authenticate_user(user)
+        book_params = {
+          book_name: '',
+          author: '',
+          published_date: nil,
+          category_id: nil,
+          publisher_id: nil
+        }
+        post books_path, params: { book: book_params }.to_json, headers: auth_headers.merge({'Content-Type' => 'application/json'})
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
