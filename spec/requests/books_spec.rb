79fee632427f15d30a49d6348b5d7d79c968fdf6
Denio B. Junior <deniojr@gmail.com>
2024-02-29 08:28:06 -0300

    RSpec book requests test working

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -10,8 +10,6 @@ RSpec.describe 'Books', type: :request do
     @publisher = Publisher.find_or_create_by(id: 5, name: 'Penguin Random House')
   end
 
-
-
   describe 'POST /books' do
     context 'when user is logged in' do
       it 'creates a new Book' do
@@ -38,6 +36,20 @@ RSpec.describe 'Books', type: :request do
         }
         post books_path, params: { book: book_params }.to_json, headers: auth_headers.merge({'Content-Type' => 'application/json'})
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'when user is logged NOT in' do
+      it 'does NOT creates a new Book' do
+        book_params = {
+          book_name: 'The Factory Bot Book 2',
+          author: 'Factory Bot',
+          published_date: Date.current,
+          category_id: @category.id,
+          publisher_id: @publisher.id
+        }
+        post books_path, params: { book: book_params }.to_json
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
